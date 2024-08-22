f3b2bc707b538d422b4f35bcccbbb176fc3018a8
Hamedissm <omidi0258@gmail.com>
2024-08-22 13:11:51 +0330

    continue all pages except login

--- src/components/titlePage.jsx
+++ src/components/titlePage.jsx
@@ -1,9 +1,19 @@
-import { Children } from 'react';
+import { useEffect, useState } from 'react'
 import styles from './titlePage.module.css'
 
 export default function TitlePage({Children,imageSource,width,height,bgcolor,dir,highlightColor}){
+    const [flexDir,SetFlexDir] = useState(dir)
+    useEffect(()=>{
+        if(window.screen.availWidth>= 768){
+            SetFlexDir(dir)
+        }else{
+            SetFlexDir('column')
+        }
+        console.log(window.screen);
+    },[])
+    
     return(
-        <section className={styles.title_of_page} style={{backgroundColor: bgcolor , flexDirection: dir}}>
+        <section className={styles.title_of_page} style={{backgroundColor: bgcolor , flexDirection: flexDir}}>
             <div className={styles.text}>{Children}</div>
             <div className={styles.image} style={{width:width,height:height,backgroundColor:highlightColor}}>
                 <img src={imageSource} />
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,16 @@
 import Header from "../components/header";
 import TitlePage from "../components/titlePage";
-import TitleImg from '../assets/images/hamed (2).jpg'
+import TitleImg from '../assets/images/hamed (2).jpg';
+import ComunityImg from '../assets/images/community.jpg'
 import SupportedBy from "../components/supported";
+import LinkPages from "../components/sectionLinkPages";
+import Footer from "../components/footer";
 
 export default function Home(){
     return(
         <>
             <Header/>
-            <TitlePage imageSource={TitleImg} bgcolor={'rgb(3, 6, 54)'} highlightColor={'rgb(6, 12, 101)'} height={'350px'} Children={
+            <TitlePage imageSource={TitleImg} dir={'row'} bgcolor={'rgb(3, 6, 54)'} highlightColor={'rgb(6, 12, 101)'} height={'350px'} Children={
                 <>
                     <h1>Home page</h1>
                     <div>In publishing and graphic design, Lorem ipsumis a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, without the meaning of the text influencing the design.</div>
@@ -20,6 +23,14 @@ export default function Home(){
                     <div>In publishing and graphic design, Lorem ipsumis a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, without the meaning of the text influencing the design.</div>
                 </>
             }/>
+            <LinkPages/>
+            <TitlePage imageSource={ComunityImg} dir={'row'} bgcolor={'rgb(3, 6, 54)'} highlightColor={'rgb(6, 12, 101)'} height={'350px'} Children={
+                <>
+                    <h1>Join Our Community</h1>
+                    <p>We reduce cost of living and help you meet your most important living expectations by increasing izy access to all the necessary resources needed to improve living standards.</p>
+                </>
+            }/>
+            <Footer/>
         </>
     )
 }
